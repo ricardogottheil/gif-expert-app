c6c707b0e5fd13fe6c0a926d4dd7c4a24431717f
Ricardo Gottheil <ricardogottheil@gmail.com>
2020-09-11 17:29:21 -0500

    docs added

---
+++ docs/precache-manifest.efb8d41f78dddbc11d89d3005756d1ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83451e0e2291e29e1391c2c2f3738951",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df8f421d516806a4cb8d",
+    "url": "/static/css/main.456b4da3.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df8f421d516806a4cb8d",
+    "url": "/static/js/main.802273a4.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
